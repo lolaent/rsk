1182a6bb302b49f754b9b58bebda62b8bf84e98c
Rupert Redington <rupert@neontribe.co.uk>
2020-10-04 18:18:08 +0100

    feat: hello world

--- examples/hello-world/src/index.js
+++ examples/hello-world/src/index.js
@@ -1,27 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Decorations = (props) => {
-  console.log(props);
-  return (
-    <>
-      {props.showMarquee && <marquee>{props.name}</marquee>}
-      {props.name === 'earthware' && <img src="https://earthware.co.uk/Content/img/shared/menu-logo-2x.png" alt="earthware" />}
-    </>
-  );
-}
-
-const Greeter = ({name, decorate=false}) => {
-
-  return (
-    <>
-      <h1>Hello {name}</h1>
-      {decorate && <Decorations name={name} showMarquee={true} />}
-      <span>{new Date().toString()}</span>
-    </>
-  )
+function HelloWorld() {
+  return React.createElement('h1', {}, 'Hi Folks')
 }
 
 const container = document.getElementById('root');
 
-ReactDOM.render(<Greeter name="earthware" showMarquee={true}/>, container);
+ReactDOM.render(React.createElement(HelloWorld), container);
